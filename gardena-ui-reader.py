885172210825df74295d617fb93b7a36eca44c15
brandadr <brandadrian@gmail.com>
2021-04-15 08:37:49 +0200

    Initial

---
+++ gardena-ui-reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('https://smart.gardena.com/#/session/new?username=')
+
+browser.find_element_by_id("ember9").send_keys('brandadrian@gmail.com')
+browser.find_element_by_id("ember13").send_keys('sicher')
+browser.find_element_by_id("login-button").click()
+
+items = browser.find_elements_by_tag_name("a")
+
+for item in items:
+    print("a")
+    print(item.text)
+
+
+
+
